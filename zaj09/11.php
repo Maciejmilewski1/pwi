2e09494bf86eb616a7da98a6a7a94de856cf22d2
Maciejmilewski1 <47855973+Maciejmilewski1@users.noreply.github.com>
2019-04-25 13:21:19 +0200

    Add files via upload

---
+++ zaj09/11.php
@@ -0,0 +1,9 @@
+
+<?php
+session_start();
+
+$_SESSION["newsession"]='aaaa';
+
+
+?>
+<a href="12.php">12<a>
---
+++ zaj09/12.php
@@ -0,0 +1,8 @@
+<?php
+session_start();
+
+
+echo $_SESSION["newsession"];
+
+?>
+<a href="13.php">13</a>
