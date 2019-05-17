c00ea3891902699af2efa062a7acb01718f23bc3
Maciejmilewski1 <47855973+Maciejmilewski1@users.noreply.github.com>
2019-05-17 14:58:19 +0200

    Add files via upload

---
+++ zaj10/17.php
@@ -0,0 +1,6 @@
+<form method="post" action="18.php">
+<textarea name="text">
+</textarea>
+<input type="submit" value="Zapisz">
+
+</form>
---
+++ zaj10/18.php
@@ -0,0 +1,6 @@
+
+<?php
+	$fp = fopen("plik.txt", "w");
+	fputs($fp, $_POST['tekst']);
+	fclose($fp);
+?>
